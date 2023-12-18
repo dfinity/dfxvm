25c39fbcdb74ba7ccb617c53a9e5cf273c129c1b
Eric Swanson <eric.swanson@dfinity.org>
2023-12-18 11:31:42 -0800

    docs: license and acknowledgements

--- src/installation/env.rs
+++ src/installation/env.rs
@@ -1,5 +1,6 @@
 use crate::installation::dirs::{get_bin_dir_user_facing, get_data_local_dir_user_facing};
 
+// The env.sh file is copied and modified from https://github.com/rust-lang/rustup/blob/master/src/cli/self_update/env.sh
 const ENV_FILE_TEMPLATE: &str = include_str!("env.sh");
 
 pub fn get_env_path_user_facing() -> String {
